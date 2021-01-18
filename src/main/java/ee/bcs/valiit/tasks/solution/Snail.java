868c3ddabfa4a69b5ddc397b5331dba3802564dd
Reino Bürkland <reino@byrkland.ee>
2021-01-18 12:25:50 +0200

    snail

---
+++ src/main/java/ee/bcs/valiit/tasks/solution/Snail.java
@@ -0,0 +1,20 @@
+package ee.bcs.valiit.tasks.solution;
+
+public class Snail {
+
+    public static void main(String[] args) {
+        int[][] test = {{1,2,3},{4,5,6},{7,8,9}};
+        snail(test);
+    }
+
+    public static int[] snail(int[][] array) {
+
+        int size = array.length -1;
+        for (int i = 0; i<size;i++){
+
+        }
+
+        int[] proov = {1,2,3,4,5,6,7,8,9};
+        return proov;
+    }
+}
